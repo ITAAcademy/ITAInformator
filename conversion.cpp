a457e25c05a620fe2f72fb733f1a8a99e389e09f
Lenka2023 <Lenka2023@users.noreply.github.com>
2014-10-30 11:00:58 +0200

    Update conversion.cpp
    add includes

--- conversion.cpp
+++ conversion.cpp
@@ -1,11 +1,15 @@
 #include "conversion.h"
 #include "Units/unit.h"
 #include "Units/formula.h"
+#include "conio.h"
+#include <iostream>
+using namespace std;
 
 Conversion::Conversion()
 {
     int C; int F;
     QString name;
 C=5*(F-32)/9;
-cout<<C<<endl;
+cout<<C<<endl
+getch();
 }
